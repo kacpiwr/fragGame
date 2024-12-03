5427e05d4ff1ccfe7ba910ed0d092aeec2fb12f8
Kacper Wrobel <kacper.a.wrobel@gmai.com>
2024-12-03 13:20:56 +0100

    levels and stork

--- frogGame.c
+++ frogGame.c
@@ -25,11 +25,17 @@
 #define CAR_COLOR_FRIENDLY	6
 #define LOG_COLOR	7
 #define TERMINAL_COLOR 8
+#define STORK_COLOR 9
 
 #define BORDER		1
 #define DELAY_ON	1
 #define DELAY_OFF	0
 
+struct cords 
+{
+	int x;
+	int y;
+};
 
 struct car
 {
@@ -40,11 +46,6 @@ struct car
 	int direction;
 	int color;
 	int friendly;
-};
-struct log
-{
-	int x;
-	int y;
 };
 struct ranking{
 	int first_place;
@@ -83,6 +84,7 @@ WINDOW* Start()
 	init_pair(CAR_COLOR_FRIENDLY, COLOR_YELLOW, COLOR_WHITE);
 	init_pair(LOG_COLOR, COLOR_GREEN, COLOR_WHITE);
 	init_pair(TERMINAL_COLOR, COLOR_BLACK, COLOR_WHITE);
+	init_pair(STORK_COLOR, COLOR_WHITE, COLOR_RED);
 
 	noecho();								// Switch off echoing, turn off cursor
 	curs_set(0);
@@ -128,6 +130,24 @@ void drawCar(int x, int y, int color,  WIN *playwin){
 	mvwprintw(playwin->window, x+1, y,"(____)");
 	mvwprintw(playwin->window, x+2, y, " O  O ");
 }
+void delStork(int x, int y, WIN *playwin){
+	if((x)%4 == 0){
+		wattron(playwin->window, COLOR_PAIR(PAVEMENT_COLOR));
+		wrefresh(playwin->window);
+		mvwprintw(playwin->window, x, y, "---");
+	}else{
+		wattron(playwin->window, COLOR_PAIR(ROAD_COLOR));
+		wrefresh(playwin->window);
+		mvwprintw(playwin->window, x, y, "   ");
+	}
+	
+}
+void drawStork(int x, int y,  WIN *playwin){
+
+	wattron(playwin->window, COLOR_PAIR(STORK_COLOR));
+	wrefresh(playwin->window);
+	mvwprintw(playwin->window, x, y, "<^>");
+}
 struct car newCar(){
 	int x;
 	if (rand()%2){
@@ -144,7 +164,7 @@ struct car newCar(){
 	}
 	return new;
 }
-struct log newlog(WIN *playwin){
+struct cords newlog(WIN *playwin){
 	int x = rand()%(COLUMNS-20)+10;
 	int y = 5+4*(1+rand()%7); 
 	wattron(playwin->window, COLOR_PAIR(LOG_COLOR));
@@ -152,10 +172,10 @@ struct log newlog(WIN *playwin){
 	mvwprintw(playwin->window, y, x, "  0  ");
 	mvwprintw(playwin->window, y+1, x," 0 0 ");
 	mvwprintw(playwin->window, y+2, x, "0 0 0");
-	struct log new = {x, y};
+	struct cords new = {x, y};
 	return new;
 }
-int drivingCar(struct car current, WIN *playwin, struct log logs[NUMBER_OF_LOGS]){
+int drivingCar(struct car current, WIN *playwin, struct cords logs[NUMBER_OF_LOGS]){
 	nanosleep(&current.speed, NULL);
 	for(int i = 0; i<NUMBER_OF_LOGS; i++){
 		if (current.x == logs[i].y && current.y+CARWIDTH+current.direction>=logs[i].x && current.y+current.direction<=logs[i].x+5){
@@ -170,7 +190,7 @@ int drivingCar(struct car current, WIN *playwin, struct log logs[NUMBER_OF_LOGS]
 	return 0;
 
 }
-int drivingTaxi(struct car current, WIN *playwin, struct log logs[NUMBER_OF_LOGS],int x,int y){
+int drivingTaxi(struct car current, WIN *playwin, struct cords logs[NUMBER_OF_LOGS],int x,int y){
 	nanosleep(&current.speed, NULL);
 	for(int i = 0; i<NUMBER_OF_LOGS; i++){
 		if (current.x == logs[i].y && current.y+CARWIDTH+current.direction>=logs[i].x && current.y+current.direction<=logs[i].x+5){
@@ -185,7 +205,22 @@ int drivingTaxi(struct car current, WIN *playwin, struct log logs[NUMBER_OF_LOGS
 	delFrog(x, y, playwin);
 	drawFrog(x, y+=current.direction, playwin);
 	return 0;
-
+}
+struct cords flyStork(struct cords stork, struct cords frog, WIN *playwin){
+	struct cords output = {stork.x, stork.y};
+	if(stork.x > frog.x){
+		output.x -= 1;
+	}else if(stork.x <frog. x){
+		output.x += 1;
+	}
+	if(stork.y < frog.y){
+		output.y += 1;
+	}else if(stork.y > frog.y){
+		output.y -= 1;
+	}
+	delStork(stork.x, stork.y, playwin);
+	drawStork(output.x, output.y, playwin);
+	return output;
 }
 
 
@@ -238,7 +273,6 @@ struct ranking getRanking(int score){
 	int first=0, second=0, third=0;
 	FILE *readRanking  = fopen("ranking.txt", "r");
 	fscanf(readRanking, "%d %d %d", &first, &second, &third);
-	
 	if(score>first) {
 		struct ranking current = {score, first,second};
 		FILE *writeRanking  = fopen("ranking.txt", "w");
@@ -269,13 +303,71 @@ struct ranking getRanking(int score){
 		return current;
 	}
 }
-void mainLoop(int ch, int x, int y, struct timespec sleeptime, WIN *playwin){
-	int number_of_logs, number_of_cars;
-	FILE *paremeters  = fopen("parameters.txt", "r");
-	fscanf(paremeters, "%d %d", &number_of_cars, &number_of_logs); 
-	fclose(paremeters);
+void endGame(WIN *playwin, int score){
+	clear();
+	wattron(playwin->window, COLOR_PAIR(TERMINAL_COLOR));
+	move(ROWS/2,COLUMNS/2-4);
+	addstr("GAME OVER");
+	mvwprintw(playwin->window, ROWS/2+2,COLUMNS/2-4, "SCORE: %d", score);
+	struct ranking current_ranking = getRanking(score);
+	mvwprintw(playwin->window, ROWS/2+3,COLUMNS/2-4, "1: %d", current_ranking.first_place);
+	mvwprintw(playwin->window, ROWS/2+4,COLUMNS/2-4, "2: %d", current_ranking.second_place);
+	mvwprintw(playwin->window, ROWS/2+5,COLUMNS/2-4, "3: %d", current_ranking.third_place);
+	getch();
+	struct timespec timeafterdeath = {4,2};
+	nanosleep(&timeafterdeath, NULL);
+}
+struct cords moveFrog(WIN *playwin, int ch, struct timespec sleeptime, struct cords input){
+	struct cords frog = input;
+	switch (ch)
+			{
+			case KEY_LEFT:
+				if(frog.y-FROGWIDTH>=1 && frog.y-FROGWIDTH<=COLUMNS-1){
+					delFrog(frog.x, frog.y, playwin);
+					frog.y-=FROGWIDTH;
+					drawFrog(frog.x, frog.y, playwin);
+					flushinp(); 
+					nanosleep(&sleeptime, NULL);
+				}
+				break;
+			case KEY_RIGHT:
+				if(frog.y+FROGWIDTH>=1 && frog.y+FROGWIDTH<=COLUMNS-1){
+					delFrog(frog.x, frog.y, playwin);
+					frog.y+=FROGWIDTH;
+					drawFrog(frog.x, frog.y, playwin);
+					flushinp();  
+					nanosleep(&sleeptime, NULL);
+				}
+				break;
+			case KEY_UP:
+				if(frog.x-FROGHEIGHT>=1 && frog.x-FROGHEIGHT+1<=ROWS-1){
+					delFrog(frog.x, frog.y, playwin);
+					frog.x-=FROGHEIGHT+1;
+					drawFrog(frog.x, frog.y, playwin);
+					flushinp();  
+					nanosleep(&sleeptime, NULL);
+				}
+				break;
+			case KEY_DOWN:
+				if(frog.x+FROGHEIGHT>=1 && frog.x+FROGHEIGHT+1<=ROWS-1){
+					delFrog(frog.x, frog.y, playwin);
+					frog.x+=FROGHEIGHT+1;
+					drawFrog(frog.x, frog.y, playwin);
+					flushinp();  
+					nanosleep(&sleeptime, NULL);
+				}
+				break;
+			
+			}
+	return frog;
+}
+int nextLevel(int currentScore,int currentLevel, int ch, int x, int y, struct timespec sleeptime, WIN *playwin,int number_of_cars,int number_of_logs);
+void mainLoop(int ch, int x, int y, struct timespec sleeptime, WIN *playwin,int number_of_cars,int number_of_logs,int newLevel, int newScore){
+	struct cords frog = {x, y};
+	struct cords stork = {2, ROWS/2};
 	struct car* cars = malloc(number_of_cars * sizeof(struct car));
-	struct log* logs = malloc(number_of_logs * sizeof(struct log));
+	struct cords* logs = malloc(number_of_logs * sizeof(struct cords));
+	int level = newLevel;
 	for(int i=0; i<number_of_cars; i++) {
 		cars[i] = newCar();
 	}
@@ -284,13 +376,21 @@ void mainLoop(int ch, int x, int y, struct timespec sleeptime, WIN *playwin){
 	}
 	int end = 0;
 	int alive = 1;
-	int score = 0;
+	int score = newScore;
 	do{
+		if(nextLevel(score , level, ch,  x,  y,   sleeptime,  playwin, number_of_cars, number_of_logs) == 1){
+			alive=0;
+			end=1;
+			free(cars);
+			free(logs);
+		}
+		struct cords new =flyStork(stork, frog, playwin);
+		stork = new;
 		for(int i=0; i<number_of_cars; i++) {
-			if(cars[i].friendly!=0 || isClose(x,cars[i].x, y, cars[i].y)==0){
-				if(cars[i].friendly == 1 && isCollision(x,cars[i].x, y, cars[i].y)){
-					drivingTaxi(cars[i], playwin, logs, x, y);
-					y+=cars[i].direction;
+			if(cars[i].friendly!=0 || isClose(frog.x,cars[i].x, frog.y, cars[i].y)==0){
+				if(cars[i].friendly == 1 && isCollision(frog.x,cars[i].x, frog.y, cars[i].y)){
+					drivingTaxi(cars[i], playwin, logs, frog.x, frog.y);
+					frog.y+=cars[i].direction;
 				}
 				else{
 					if (drivingCar(cars[i], playwin, logs) == 1){
@@ -300,25 +400,11 @@ void mainLoop(int ch, int x, int y, struct timespec sleeptime, WIN *playwin){
 						cars[i].y+=cars[i].direction;
 					}
 				}
-				
 			}
-				
-			if(((isCollision(x,cars[i].x, y, cars[i].y)) && cars[i].friendly != 1) || (isCollision(x,logs[0].y, y, logs[0].x))|| (isCollision(x,logs[1].y, y, logs[1].x))|| (isCollision(x,logs[2].y, y, logs[2].x))){
+			if(((isCollision(frog.x,cars[i].x, frog.y, cars[i].y)) && cars[i].friendly != 1) || (isCollision(frog.x,logs[0].y, frog.y, logs[0].x))|| (isCollision(frog.x,logs[1].y, frog.y, logs[1].x))|| (isCollision(frog.x,logs[2].y, frog.y, logs[2].x)) || isCollision(stork.x, frog.x, stork.y, frog.y)){
 				alive=0;
 				end=1;
-				
-				clear();
-				wattron(playwin->window, COLOR_PAIR(TERMINAL_COLOR));
-				move(ROWS/2,COLUMNS/2-4);
-				addstr("GAME OVER");
-				mvwprintw(playwin->window, ROWS/2+2,COLUMNS/2-4, "SCORE: %d", score);
-				struct ranking current_ranking = getRanking(score);
-				mvwprintw(playwin->window, ROWS/2+3,COLUMNS/2-4, "1: %d", current_ranking.first_place);
-				mvwprintw(playwin->window, ROWS/2+4,COLUMNS/2-4, "2: %d", current_ranking.second_place);
-				mvwprintw(playwin->window, ROWS/2+5,COLUMNS/2-4, "3: %d", current_ranking.third_place);
-				getch();
-				struct timespec timeafterdeath = {4,2};
-				nanosleep(&timeafterdeath, NULL);
+				endGame(playwin, score);
 				free(cars);
 				free(logs);
 			}
@@ -329,74 +415,43 @@ void mainLoop(int ch, int x, int y, struct timespec sleeptime, WIN *playwin){
 			end = 1;
 		}
 		if (alive){
-				switch (ch)
-			{
-			case KEY_LEFT:
-				if(y-FROGWIDTH>=1 && y-FROGWIDTH<=COLUMNS-1){
-					delFrog(x, y, playwin);
-					y-=FROGWIDTH;
-					drawFrog(x, y, playwin);
-					flushinp(); 
-					nanosleep(&sleeptime, NULL);
-				}
-				break;
-			case KEY_RIGHT:
-				if(y+FROGWIDTH>=1 && y+FROGWIDTH<=COLUMNS-1){
-					delFrog(x, y, playwin);
-					y+=FROGWIDTH;
-					drawFrog(x, y, playwin);
-					flushinp();  
-					nanosleep(&sleeptime, NULL);
-				}
-				break;
-			case KEY_UP:
-				if(x-FROGHEIGHT>=1 && x-FROGHEIGHT+1<=ROWS-1){
-					delFrog(x, y, playwin);
-					x-=FROGHEIGHT+1;
-					drawFrog(x, y, playwin);
-					flushinp();  
-					nanosleep(&sleeptime, NULL);
-				}
-				break;
-			case KEY_DOWN:
-				if(x+FROGHEIGHT>=1 && x+FROGHEIGHT+1<=ROWS-1){
-					delFrog(x, y, playwin);
-					x+=FROGHEIGHT+1;
-					drawFrog(x, y, playwin);
-					flushinp();  
-					nanosleep(&sleeptime, NULL);
-				}
-				break;
 			
-			}
-			
-			if(x < 4){
-				delFrog(x, y, playwin);
-				x=ROWS-FROGHEIGHT - 1;
-				y=CENTERY;
-				drawFrog(x, y, playwin);
+			frog = moveFrog(playwin, ch, sleeptime, frog);
+			if(frog.x < 4){
+				delFrog(frog.x, frog.y, playwin);
+				frog.x=ROWS-FROGHEIGHT - 1;
+				frog.y=CENTERY;
+				drawFrog(frog.x, frog.y, playwin);
 				score++;
 			}
 		}
-		
-		
-		// flushinp();                     				 	 	
-
 		refresh();
-		
 	}while (end == 0);
-	
-
-
 }
+int nextLevel(int currentScore,int currentLevel, int ch, int x, int y, struct timespec sleeptime, WIN *playwin,int number_of_cars,int number_of_logs){
+	int score =currentScore;
+	int level =currentLevel;
+	if (currentScore>0 && currentScore%2 == 0 && currentLevel<4){
+		mvwprintw(playwin->window, ROWS/2+2,COLUMNS/2-4, "Next Level %d", currentLevel+1);
+		clear();
+		struct timespec timeafterdeath = {2,2};
+		nanosleep(&timeafterdeath, NULL);
+		DrawBoard(playwin);
+		int x = ROWS-FROGHEIGHT - 1, y = COLUMNS/2;
+		drawFrog(x, y, playwin);
+		mainLoop(ch,  x,  y,   sleeptime,  playwin, number_of_cars+5, number_of_logs+2, level+1, score+1);
+		return 1;
+		clear();
+		endwin();
 
+	}
+	return 0;
+}
 int main(){
 	WINDOW *win = Start();
     cbreak(); 
     keypad(stdscr, TRUE);
 	curs_set(0);
-	
-	// WINDOW *win = newwin(ROWS, COLUMNS, 0, 0);
 	nodelay(win, TRUE);
     Welcome(win);
 	keypad(stdscr, TRUE);
@@ -410,8 +465,12 @@ int main(){
 	keypad(playwin->window, TRUE);
 	DrawBoard(playwin);
 	drawFrog(x, y, playwin);
-
-	mainLoop(ch,  x,  y,   sleeptime, playwin);
+	
+	int number_of_logs, number_of_cars, level = 1;
+	FILE *paremeters  = fopen("parameters.txt", "r");
+	fscanf(paremeters, "%d %d", &number_of_cars, &number_of_logs); 
+	fclose(paremeters);
+	mainLoop(ch,  x,  y,   sleeptime, playwin, number_of_cars, number_of_logs, level, 0);
 	
 	clear();
 	endwin();
